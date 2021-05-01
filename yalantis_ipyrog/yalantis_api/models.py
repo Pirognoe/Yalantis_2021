39129aa66768844c449f5c3c4519cd09dffe44bf
Ivan Pyrog <ipirognoe@gmail.com>
2021-05-02 00:25:43 +0300

    [Add DRF]
    Added DRF to the project's apps and also configured first Model

--- yalantis_ipyrog/yalantis_api/models.py
+++ yalantis_ipyrog/yalantis_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TrainingCourse(models.Model):
+    """
+    Basic model in our app, represents a training course
+    """
+    name = models.CharField(max_length=60, primary_key=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    number_of_lectures = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- yalantis_ipyrog/yalantis_ipyrog/settings.py
+++ yalantis_ipyrog/yalantis_ipyrog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'yalantis_api.apps.YalantisApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
