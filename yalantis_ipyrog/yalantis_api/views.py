48e6812733523abc13b0dfead19a87cdcc6603c9
Ivan Pyrog <ipirognoe@gmail.com>
2021-05-02 20:08:00 +0300

    [Adding filters to the course API]
    Overriding get_queryset method to set custom filter by course name

--- yalantis_ipyrog/yalantis_api/views.py
+++ yalantis_ipyrog/yalantis_api/views.py
@@ -6,5 +6,16 @@ from .models import TrainingCourse
 
 
 class TrainingCourseViewSet(viewsets.ModelViewSet):
-    queryset = TrainingCourse.objects.all().order_by('name')
+
     serializer_class = TrainingCourseSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned training courses
+        by filtering against a `name` query parameter in the URL.
+        """
+        queryset = TrainingCourse.objects.all().order_by('name')
+        training_course_name = self.request.query_params.get('name')
+        if training_course_name is not None:
+            queryset = queryset.filter(name=training_course_name)
+        return queryset
